2280d516695af27f4c9622e0562f0b593aaae8ec
Zamir31 <noriegad258@gmail.com>
2023-04-14 06:58:38 -0600

    creando conexion

---
+++ controllers/productoControllers.js
@@ -0,0 +1,58 @@
+
+
+exports.crearProducto = async (req, res) => {
+
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+};
+
+
+exports.obtenerProductos = async (req, res) => {
+
+    try {
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+}
+
+exports.actualizarProducto = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+};
+
+exports.obtenerProducto = async (req, res) => {
+
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+};
+
+exports.deletearProducto = async (req, res) => {
+    try {
+
+        let producto = await Producto.findById(req.params.id);
+        if (!producto) {
+            res.status(404).json({ message: "Producto no existe" });
+        }
+
+        await Producto.findOneAndRemove({ _id: req.params.id });
+        res.json({ message: "Producto eliminado"});
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+};
